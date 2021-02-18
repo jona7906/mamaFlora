86ac4c071fd77e8d18d2bbb564fa867cf28b6887
jona7906 <70849321+jona7906@users.noreply.github.com>
2021-02-18 15:20:22 +0100

    js database key osv

---
+++ javascript.js
@@ -0,0 +1,23 @@
+       document.addEventListener("DOMContentLoaded", () => //tjekker inden om DOM er loaded
+           {
+
+
+               const url = "https://plante-2af1.restdb.io/rest/planter";
+               const options = {
+                   headers: {
+                       'x-apikey': "602e76225ad3610fb5bb6339"
+                   }
+               }
+
+
+
+               async function hentData() {
+                   const respons = await fetch(url, options);
+                   const json = await respons.json();
+                   vis(json);
+               }
+
+               hentData();
+
+
+           })

5e1755238cb45e3b9c7d460b5b98c647e1389972
jona7906 <70849321+jona7906@users.noreply.github.com>
2021-02-19 14:36:12 +0100

    asdaksdjklasjd

--- multi.js
+++ multi.js
@@ -14,12 +14,28 @@
                const json = await respons.json();
                vis(json);
            }
-
            hentData();
+
+           let filter = "navn";
+           const filterknapper = document.querySelectorAll(".filter button");
+
+           filterknapper.forEach(knap => knap.addEventListener("click", sorter))
+
+           function sorter() {
+               console.log("sortering");
+               filter = this.dataset.kategori;
+               document.querySelector(".valgt").classList.remove("valgt");
+               this.classList.add("valgt");
+               hentData();
+           }
 
            function vis(json) {
                console.log(json);
                const template = document.querySelector("template");
+               let multiPlanter = document.querySelector("#multiPlanter");
+
+
+
 
 
                json.forEach(planter => {
@@ -32,7 +48,16 @@
                    klon.querySelector("button.buttonPlante").addEventListener("click", visPlante);
 
                    klon.querySelector("#imgPlante").src = imgURL + planter.pic[0];
-                   planteSection.appendChild(klon);
+
+                   if (filter == "navn") {
+                       planteSection.appendChild(klon);
+                   }
+                   if (filter == "vand") {
+                       planteSection.appendChild(klon);
+                   }
+                   if (filter == "sol") {
+                       planteSection.appendChild(klon);
+                   }
                })
 
            }
